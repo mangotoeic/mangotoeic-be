79e620e822b99d694b8dce8ea7b882c4c03e276f
jongmok1031 <jongmok1031@gmail.com>
2020-11-04 15:23:58 +0900

    리뷰 부분 추가

--- mangotoeic/ext/routes.py
+++ mangotoeic/ext/routes.py
@@ -10,7 +10,7 @@ from mangotoeic.resource.recommendation import Recommendation
 from mangotoeic.resource.odap import Odap, Odaps
 from mangotoeic.resource.vocab import Vocab, Vocabs
 from mangotoeic.resource.testresult import TestResult, TestResults
-from mangotoeic.resource.review import Review, Reviews
+from mangotoeic.resource.review import Review, Reviews, ReviewSearchByEmail
 
 legacies = Blueprint('legacies', __name__, url_prefix='/api/legacies')
 legacy = Blueprint('legacy', __name__, url_prefix='/api/legacy')
@@ -28,8 +28,10 @@ access = Blueprint('access', __name__, url_prefix='/api/access')
 
 testresult = Blueprint('testresult', __name__, url_prefix='/api/testresult')
 testresults = Blueprint('testresults', __name__, url_prefix='/api/testresults')
-review = Blueprint('review', __name__, url_prefix='/api/review')
+
+review = Blueprint('review', __name__, url_prefix='/api/review/<string:id>')
 reviews = Blueprint('reviews', __name__, url_prefix='/api/reviews')
+reviewsearchbyemail = Blueprint('reviews', __name__, url_prefix='/api/reviewsearchbyemail/<string:id>')
 
 
 api = Api(legacy)
@@ -46,12 +48,14 @@ api = Api(testresult)
 api = Api(testresults)
 api = Api(review)
 api = Api(reviews)
+api = Api(reviewsearchbyemail)
 
 def initialize_routes(api):
     api.add_resource(Home, '/api')
     api.add_resource(User, '/api/user')
     api.add_resource(Users, '/api/users')
     api.add_resource(Review, '/api/review/<string:id>')
+    api.add_resource(ReviewSearchByEmail, '/api/reviewsearchbyemail/<string:id>')
     api.add_resource(Reviews, '/api/reviews/')
     api.add_resource(Legacy, '/api/legacy')
     api.add_resource(Legacies, '/api/legacies')
